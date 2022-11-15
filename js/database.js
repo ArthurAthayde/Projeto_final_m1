78b1385f5b2c8d5962c0ef7a0c4d0d071238011e
ArthurAthayde <arthur.athaydee@gmail.com>
2022-11-15 18:31:18 -0300

    ajuste na imagem carrinho

--- js/database.js
+++ js/database.js
@@ -4,7 +4,7 @@ const data = [
   {
     idNumber: 1,
     id: 'd200',
-    imgfront: "../img/D-200-Deluxe_front.png",
+    imgfront: "./img/D-200-Deluxe_front.png",
     nameItem: "D-200 Deluxe",
     description:
       "Após 184 anos, em comemoração aos *quack* 2M de numeros de séries feitos, essa versão é *quack* para ser tocada até o fim dos tempos...",
@@ -15,7 +15,7 @@ const data = [
   {
     idNumber: 2,
     id: 'lotus',
-    imgfront: "../img/LOTUS-FLOWER_front.png",
+    imgfront: "./img/LOTUS-FLOWER_front.png",
     nameItem: "D-45 Lotus",
     description:
       "Este design requintado do mestre artista de incrustações Harvey Leach apresenta flores de lótus, garças, libélulas, lírios e...",
@@ -26,7 +26,7 @@ const data = [
   {
     idNumber: 3,
     id: 'squadron',
-    imgfront: "../img/D-17_Squadron_front.png",
+    imgfront: "./img/D-17_Squadron_front.png",
     nameItem: "D-17 Squadron",
     description:
       "A obra de arte, com seu inconfundível *quack* sorriso cheio de dentes, foi executada por Robert F. Goetzl e inspirada nos...",
@@ -37,7 +37,7 @@ const data = [
   {
     idNumber: 4,
     id: 'sc2020',
-    imgfront: "../img/10CSSC2022_front.png",
+    imgfront: "./img/10CSSC2022_front.png",
     nameItem: "CS-CS-2022",
     description:
       "O CS-SC-2022 está repleto de *quack* recursos personalizados e é o primeiro instrumento fabricado na *quack* Nazareth com o revolucionário estilo de corpo SC...",
@@ -48,7 +48,7 @@ const data = [
   {
     idNumber: 5,
     id: 'm0015',
-    imgfront: "../img/00-15M_front.png",
+    imgfront: "./img/00-15M_front.png",
     nameItem: "00-15M",
     description:
       "O modelo 00-15M continua a tradição *quack* Martin com um tamanho de corpo de 00-14 trastes, construção sólida em *quack* mogno e um rico acabamento acetinado...",
@@ -59,7 +59,7 @@ const data = [
   {
     idNumber: 6,
     id: 'streetMaster',
-    imgfront: "../img/000-16_StreetMaster_front.png",
+    imgfront: "./img/000-16_StreetMaster_front.png",
     nameItem: "000-16 SMaster",
     description:
       "O popular acabamento StreetMaster® da *quack* Martin foi introduzido em sua série 15 de mogno sólido em 2017 como uma *quack* homenagem respeitosa ao músico que trabalha...",
